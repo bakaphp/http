ab209eef835bbe14b1b44198d6859fd51b8947e8
mctekk <max@mctekk.com>
2019-04-10 14:46:08 -0400

    added test

---
+++ tests/unit/UriToElasticSqlTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Baka\Http\Converter\RequestUriToSql;
+use Test\Model\Leads;
+use Phalcon\Mvc\Model\Resultset\Simple as SimpleRecords;
+use Baka\Http\Converter\RequestUriToElasticSearch;
+
+class UriToElasticSqlTest extends PhalconUnitTestCase
+{
+    /**
+     * Test a normal query with no conditional.
+     *
+     * @return boolean
+     */
+    public function testSimpleQuery()
+    {
+        //$params['q'] = ('is_delete:0');
+        $params['cq'] = ('branches.is_deleted:0;subscriptions.plan.id~3');
+        $params['limit'] = '10';
+        $params['page'] = '1';
+        $params['sort'] = 'id|desc';
+
+        $leads = new Leads();
+        $requestToSql = new RequestUriToElasticSearch($params, $leads);
+        $request = $requestToSql->convert();
+
+        print_r($request);
+        die();
+        $results = (new SimpleRecords(null, $leads, $leads->getReadConnection()->query($request['sql'], $request['bind'])));
+        $count = $leads->getReadConnection()->query($request['countSql'], $request['bind'])->fetch(\PDO::FETCH_OBJ)->total;
+
+        //confirme records
+        foreach ($results as $result) {
+            $this->assertTrue($result->id > 0);
+        }
+
+        $this->assertEquals(3, count($results->toArray()));
+        $this->assertEquals(3, $count);
+    }
+}
